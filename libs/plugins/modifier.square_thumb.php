d61aaefc57f9bd8291d1f9bcd73691273b89c75d
David <chan15tw@gmail.com>
2013-11-17 20:34:27 +0800

    fix coding style

--- libs/plugins/modifier.square_thumb.php
+++ libs/plugins/modifier.square_thumb.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * square thumb
+ * Make square thumbnail
+ *
+ * @param string $src file source
+ * @param string $path file path
+ * @param integer $ratio thumbnail ratio
+ * @param string $noFile message when file not exist
+ * @return string
  */
-function smarty_modifier_square_thumb($src, $path = '/', $ratio = 100, $noFile = '檔案不存在') {
-	include_once dirname(dirname(dirname(__FILE__))).'/libs/class.chan.php';
+function smarty_modifier_square_thumb($src, $path = '/', $ratio = 100, $noFile = '', $nameOnly = false) {
+	include_once dirname(dirname(dirname(__FILE__))) . '/libs/class.chan.php';
 	$chan = new chan;
-	return $chan->squareThumb($path, $src, $ratio, $noFile);
+	return $chan->squareThumb($path, $src, $ratio, $noFile, $nameOnly);
 }
 ?>
--- libs/plugins/modifier.thumb.php
+++ libs/plugins/modifier.thumb.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * 縮圖功能
+ * Make thumbnail
+ *
+ * @param string $src file source
+ * @param string $path file path
+ * @param integer $width thumbnail width
+ * @param integer $height thumbnail height
+ * @param string $noFile message when file not exist
+ * @param string $nameOnly return image name only if true
+ * @return string
  */
-function smarty_modifier_thumb($src, $path = '/', $width = 100, $height = 100, $noFile = '檔案不存在') {
-	include_once dirname(dirname(dirname(__FILE__))).'/libs/class.chan.php';
+function smarty_modifier_thumb($src, $path = '/', $width = 100, $height = 100, $noFile = '', $nameOnly = false) {
+	include_once dirname(dirname(dirname(__FILE__))) . '/libs/class.chan.php';
 	$chan = new chan;
-	return $chan->thumb($path, $src, $width, $height, $noFile);
+	return $chan->thumb($path, $src, $width, $height, $noFile, $nameOnly);
 }
 ?>

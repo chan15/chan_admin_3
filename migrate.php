725b214aa501e3c8a20c3901e1c0d051eb915ae1
David <chan15tw@gmail.com>
2014-02-27 15:33:53 +0800

    database migration function

---
+++ migrate.php
@@ -0,0 +1,36 @@
+<?php
+include 'main.php';
+$path = 'db/';
+$chan->connect();
+
+// Check if migrations table exists
+$sql = "DESCRIBE `migrations`";
+$result = $chan->sqlExecute($sql);
+
+if (false === $result) {
+    $sql = file_get_contents($path . 'migrations');
+    $chan->sqlExecute($sql);
+}
+
+// Start reading file and check
+if ($handle = opendir($path)) {
+    while (false !== ($entry = readdir($handle))) {
+        if (false === is_dir($entry) && 'migrations' !== $entry) {
+            $sql = sprintf("SELECT * FROM `migrations` WHERE `name` = %s",
+                $chan->toSql($entry, 'text'));
+            $row = $chan->myOneRow($sql);
+
+            if (NULL === $row) {
+                $sql = file_get_contents($path . $entry);
+                $chan->sqlExecute($sql);
+
+                $chan->table = 'migrations';
+                $chan->addField('name', $entry);
+                $chan->addField('created_at', $chan->retNow(), 'date');
+                $chan->save();
+            }
+        }
+    }
+
+    closedir($handle);
+}

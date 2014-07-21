d2b5514801b7a5242186f8b7cf70464abaa748a0
Chan <chan15tw@gmail.com>
2014-07-21 11:37:46 +0800

    coding style

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(0);
 header('Content-type: text/html; charset=utf-8');
 include 'const.php';
@@ -8,10 +9,10 @@ include'libs/Smarty.class.php';
 $smarty = new Smarty;
 
 $path = dirname(__FILE__);
-$smarty->template_dir = $path.'/templates/';
-$smarty->compile_dir = $path.'/templates_c/';
-$smarty->config_dir = $path.'/configs/';
-$smarty->cache_dir = $path.'/cache/';
+$smarty->template_dir = $path . '/templates/';
+$smarty->compile_dir = $path . '/templates_c/';
+$smarty->config_dir = $path . '/configs/';
+$smarty->cache_dir = $path . '/cache/';
 
 include_once 'libs/class.chan.php';
 $chan = new Chan;
@@ -22,4 +23,3 @@ $chan->username = DB_USERNAME;
 $chan->password = DB_PASSWORD;
 
 $chan->sessionOn();
-?>

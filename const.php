af330b0edd4ff058cbec1545bdb056f8f0fdff5c
David <chan15tw@gmail.com>
2014-10-01 00:05:04 +0800

    change protocal name

--- const.php
+++ const.php
@@ -2,4 +2,4 @@
 
 define('ADMIN_TITLE', 'name');
 define('NO_DATA', 'No data');
-define('SITE_HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
+define('SITE_HOST', 'http://' . $_SERVER['HTTP_HOST']);
